c4a446121181e6384b4e80ef0d76b549b5e8bc19
radiant4010u <43864875+kaylaradf@users.noreply.github.com>
2024-09-27 01:30:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
     
     // This will replace the result of any mathematical operation with "I love you"
     if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'balikan yuk...';
+        display.value = 'km cantik bgt deh';
     } else {
         try {
             display.value = eval(expression);
